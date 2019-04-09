97c5369e3c6667d695179d6a2fcf6e4c26d5c811
Alisher <todjiev@gmail.com>
2019-04-09 20:31:04 +0700

    fix css

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
-import {
-  withStyles,
-  MuiThemeProvider
-} from "@material-ui/core/styles";
+import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import styles from "./app-style";
 import Tomato from "../tomato";
 import NewTomato from "../new-tomato";
 import theme from "../theme/theme";
-import AlignItemsList from '../list/list'
-import withPomodoroService from '../hoc/with-pomodoro-service'
-class App extends Component {
-	render() {
-		const { classes } = this.props;
-		console.log(this.props)
+import AlignItemsList from "../list/list";
+import withPomodoroService from "../hoc/with-pomodoro-service";
 
-		return (
-			<MuiThemeProvider theme={theme}>
-				<div className={classes.root}>
-					<NewTomato />
-					<Tomato />
-					<AlignItemsList/>				
-				</div>
-			</MuiThemeProvider>
-		);
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    console.log(this.props);
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+        >
+          <div className={classes.root}>
+            <NewTomato />
+            <Tomato />
+            <AlignItemsList />
+          </div>
+        </Grid>
+      </MuiThemeProvider>
+    );
   }
 }
 
