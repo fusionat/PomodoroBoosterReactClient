d2297d23fb839cf3b50437610ea537d27f7a5126
Alisher <todjiev@gmail.com>
2019-04-11 00:10:19 +0700

    list refactoring

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,33 +1,30 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
-import { compose } from 'redux'
+import { connect } from "react-redux";
+import { compose } from "redux";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
 import styles from "./list-style";
-import TomatoListItem from '../list-item/list-item'
-import withPomodoroService from '../hoc/with-pomodoro-service' 
-import { tomatosLoaded } from '../../actions'
+import TomatoListItem from "../list-item/list-item";
+import withPomodoroService from "../hoc/with-pomodoro-service";
+import { tomatosLoaded } from "../../actions";
 
 class TomatoItemsList extends Component {
-
-  componentDidMount(){      
+  componentDidMount() {
     const { pomodoroService, tomatosLoaded } = this.props;
     tomatosLoaded(pomodoroService.getTomatos());
   }
 
-  render(){
+  render() {
     const { classes, tomatos } = this.props;
     return (
       <Card className={classes.card}>
         <CardContent>
           <List className={classes.root}>
             {tomatos.map(tomato => {
-              return (
-                <TomatoListItem data={tomato}/>
-              );
+              return <TomatoListItem data={tomato} />;
             })}
           </List>
         </CardContent>
@@ -43,11 +40,18 @@ TomatoItemsList.propTypes = {
 const mapStateToProps = ({ tomatos }) => {
   return {
     tomatos
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
   tomatosLoaded
-}
+};
 
-export default compose(withPomodoroService(), connect(mapStateToProps, mapDispatchToProps),withStyles(styles))(TomatoItemsList);
+export default compose(
+  withPomodoroService(),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withStyles(styles)
+)(TomatoItemsList);
