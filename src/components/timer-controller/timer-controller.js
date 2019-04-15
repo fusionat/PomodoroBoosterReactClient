8bdec586db05d8f0d09b639812b1894ddd6857c8
Alisher <todjiev@gmail.com>
2019-04-15 13:17:05 +0700

    Add logic to timerController

--- src/components/timer-controller/timer-controller.js
+++ src/components/timer-controller/timer-controller.js
@@ -1,29 +1,94 @@
-import React  from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import Pause from '@material-ui/icons/Pause';
-import Stop from '@material-ui/icons/Stop';
-import styles from './timer-controller-style'
-import IconButton from '@material-ui/core/IconButton';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { withStyles } from "@material-ui/core/styles";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import Pause from "@material-ui/icons/Pause";
+import Stop from "@material-ui/icons/Stop";
+import EasyTimer from "easytimer.js";
+import styles from "./timer-controller-style";
+import IconButton from "@material-ui/core/IconButton";
+import withPomodoroService from "../hoc/with-pomodoro-service";
+import { timerValueUpdated } from "../../actions";
 
-const TimeController = (props) => {
-    const { classes } = props;
-    
+const TimeController = props => {
+  const { classes, onStart, onPause, onStop } = props;
+
+  return (
+    <React.Fragment>
+      <IconButton aria-label="Start" onClick={() => onStart()}>
+        <PlayArrowIcon className={classes.play} />
+      </IconButton>
+
+      <IconButton aria-label="pause" onClick={() => onPause()}>
+        <Pause className={classes.pause} />
+      </IconButton>
+
+      <IconButton aria-label="stop" onClick={() => onStop()}>
+        <Stop className={classes.stop} />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+
+class TimeControllerContainer extends Component {
+  componentDidMount() {
+    this.timer = new EasyTimer();
+  }
+
+  componentWillUnmount() {
+    this.timer.stop();
+    this.timer = null;
+  }
+
+  onStart = () => {
+    const { timerValueUpdated } = this.props;
+    this.timer.addEventListener("secondsUpdated", e => {
+      timerValueUpdated(this.timer.getTimeValues().toString());
+    });
+    this.timer.start();
+  };
+
+  onPause = () => {
+    this.timer.pause();
+  };
+
+  onStop = () => {
+    this.timer.stop();
+  };
+
+  componentWillUnmount() {
+    this.timer.stop();
+    this.timer = null;
+  }
+
+  render() {
+    let { timerValue } = this.props;
     return (
-        <React.Fragment>
-            <IconButton aria-label="Start" onClick={()=>{alert(1);}}>
-                <PlayArrowIcon className={classes.play} />
-            </IconButton>
-
-            <IconButton aria-label="pause">
-                <Pause className={classes.pause} />
-            </IconButton>
-
-            <IconButton aria-label="stop">
-                <Stop className={classes.stop} />
-            </IconButton>
-        </React.Fragment>
+      <TimeController
+        timerValue={timerValue}
+        onStart={this.onStart}
+        onPause={this.onPause}
+        onStop={this.onStop}
+        {...this.props}
+      />
     );
+  }
 }
 
-export default withStyles(styles)(TimeController)
+const mapStateToProps = ({ timerValue }) => {
+  return { timerValue };
+};
+
+const mapDispatchToProps = {
+  timerValueUpdated
+};
+
+export default compose(
+  withPomodoroService(),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withStyles(styles)
+)(TimeControllerContainer);
--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
-
-import EasyTimer from "easytimer.js";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import withPomodoroService from "../hoc/with-pomodoro-service";
 import styles from "./timer-style";
-import { timerValueUpdated } from "../../actions";
 
 class Timer extends Component {
   render = () => {
     const { classes, timerValue } = this.props;
-
     return (
       <CardContent className={classes.timer}>
         <Typography className={classes.timerText}>{timerValue}</Typography>
@@ -24,17 +20,6 @@ class Timer extends Component {
 
 class TimeContainer extends Component {
   componentDidMount() {
-    const { timerValueUpdated } = this.props;
-    this.timer = new EasyTimer();
-    this.timer.start();
-    this.timer.addEventListener("secondsUpdated", e => {
-        timerValueUpdated(this.timer.getTimeValues().toString());
-    });
-  }
-
-  componentWillUnmount() {
-    this.timer.stop();
-    this.timer = null;
   }
 
   render() {
@@ -47,15 +32,11 @@ const mapStateToProps = ({ timerValue }) => {
   return { timerValue };
 };
 
-const mapDispatchToProps = {
-  timerValueUpdated
-};
 
 export default compose(
   withPomodoroService(),
   connect(
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
   ),
   withStyles(styles)
 )(TimeContainer);
