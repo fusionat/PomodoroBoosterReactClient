b217099aecddfb995f316278a84dc4ed443990b7
Alisher <todjiev@gmail.com>
2019-04-16 20:53:04 +0700

    Add animation

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -3,21 +3,24 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
+import Slide from '@material-ui/core/Slide';
 import styles from "./list-style";
 import tomatoIcon from "../icons/tomato.svg";
 
 const TomatoListItem = props => {
   const { data, classes } = props;
   return (
-    <ListItem className={classes.li} key={data.id}>
-      <img alt="Tomato Icon" className={classes.avatarImage} src={tomatoIcon} />
-      <ListItemText
-        primary={data.name}
-      />
-      <Typography component="span" className={classes.timerText}>
-        {data.timerValue}
-      </Typography>
-    </ListItem>
+    <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+      <ListItem className={classes.li} key={data.id}>
+        <img alt="Tomato Icon" className={classes.avatarImage} src={tomatoIcon} />
+        <ListItemText
+          primary={data.name}
+        />
+        <Typography component="span" className={classes.timerText}>
+          {data.timerValue}
+        </Typography>
+      </ListItem>
+    </Slide>
   );
 };
 
